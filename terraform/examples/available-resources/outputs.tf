2b28cb963a9c0af5555208b1e6ba3f65f747b104
Mark Watkins <mark.watkins@hpe.com>
2020-04-22 16:19:46 +0000

    Revised some documentation

--- terraform/examples/available-resources/outputs.tf
+++ terraform/examples/available-resources/outputs.tf
@@ -1,32 +1,32 @@
 
-output "infrastructure" {
-  value = "${data.quake_available_resources.physical}"
+# output "infrastructure" {
+#   value = "${data.quake_available_resources.physical}"
+# }
+
+output "locations" {
+    value = data.quake_available_resources.physical.locations
 }
 
-# output "locations" {
-#     value = data.quake_available_resources.physical.locations
-# }
+output "images" {
+    value = data.quake_available_resources.physical.images
+}
 
-# output "images" {
-#     value = data.quake_available_resources.physical.images
-# }
+output "ssh-keys" {
+    value = data.quake_available_resources.physical.ssh_keys
+}
 
-# output "ssh-keys" {
-#     value = data.quake_available_resources.physical.ssh_keys
-# }
+output "networks" {
+    value = [for net in data.quake_available_resources.physical.networks : net if net.location == var.location]
+}
 
-# output "networks" {
-#     value = data.quake_available_resources.physical.networks #[for net in data.quake_available_resources.physical.networks : net if net.location == var.location]
-# }
+output "volumes" {
+    value = [for vol in data.quake_available_resources.physical.volumes : vol if vol.location == var.location]
+}
 
-# output "volumes" {
-#     value = [for vol in data.quake_available_resources.physical.volumes : vol if vol.location == var.location]
-# }
+output "volume-flavors" {
+    value = data.quake_available_resources.physical.volume_flavors
+}
 
-# output "volume-flavors" {
-#     value = data.quake_available_resources.physical.volume_flavors
-# }
-
-# output "machine-sizes" {
-#     value = data.quake_available_resources.physical.machine_sizes 
-# }
+output "machine-sizes" {
+    value = data.quake_available_resources.physical.machine_sizes 
+}
