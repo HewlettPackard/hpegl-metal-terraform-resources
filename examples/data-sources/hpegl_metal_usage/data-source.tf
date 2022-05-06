f5a8a8da187aed7e401bd445c2e09263a213bb2a
chaitra.mylarappachar <chaitra.mylarappachar@hpe.com>
2022-05-06 18:37:15 +0000

    Addr review comments

--- examples/data-sources/hpegl_metal_usage/data-source.tf
+++ examples/data-sources/hpegl_metal_usage/data-source.tf
@@ -4,13 +4,8 @@ variable "start" {
   default = "2020-01-13T07:44:00Z"
 }
 
-variable "end" {
-  default = "2020-04-13T07:44:00Z"
-}
-
 data "hpegl_metal_usage" "used" {
   start = var.start
-  #end = var.end
 }
 
 output "compute_consumption" {
