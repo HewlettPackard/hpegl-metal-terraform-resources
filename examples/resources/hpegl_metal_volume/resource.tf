fecc27b54fb9ff020e0dbd71711553393a4c0c30
Anuradha Bose <anuradha,bose@hpe.com>
2023-09-25 23:09:59 +0000

    lint error

--- examples/resources/hpegl_metal_volume/resource.tf
+++ examples/resources/hpegl_metal_volume/resource.tf
@@ -11,7 +11,7 @@ variable "location" {
 }
 
 resource "hpegl_metal_volume" "test_vols" {
-  count            = 1
+  count             = 1
   name              = "vol-${count.index}"
   size              = 20
   shareable         = true
